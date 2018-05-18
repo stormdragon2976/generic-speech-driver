4581b3b7480a6db996f73ca379544a5fc899faf0
Storm Dragon <stormdragon2976@gmail.com>
2018-05-18 15:12:17 -0400

    Copyright stuff added.

--- orca-generic-speech-driver.py
+++ orca-generic-speech-driver.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# Copyright 2018, F123 Consulting, <information@f123.org>
+# Copyright 2018, Storm Dragon, <storm_dragon@linux-a11y.org>
+# Copyright 2018, Chrys <chrys@linux-a11y.org>
+#
+# This is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package; see the file COPYING.  If not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA. 
 
 # generic speech driver
 
